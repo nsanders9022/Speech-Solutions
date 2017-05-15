4a6bc5549753ca3f854601e2750eccbb1c8e31c8
Nicole Sanders <nsanders9022@gmail.com>
2017-05-15 16:45:20 -0700

    add start of carosel on index page

--- src/Speech/wwwroot/js/scripts.js
+++ src/Speech/wwwroot/js/scripts.js
@@ -1,1 +1,18 @@
-﻿
+﻿$(document).ready(function () {
+    //$('.carousel').carousel({
+    //    interval: 2000
+    //})
+
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item").click(function () {
+        $("#myCarousel").carousel(1);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function () {
+        $("#myCarousel").carousel("prev");
+    });
+});
