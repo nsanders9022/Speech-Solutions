0dd5f0450ac347dc6e9491502b01573bb11992d5
Nicole Sanders <nsanders9022@gmail.com>
2017-06-25 14:21:27 -0700

    change timeline for milestones

--- src/Speech/wwwroot/js/scripts.js
+++ src/Speech/wwwroot/js/scripts.js
@@ -22,13 +22,3 @@
     });
 });
 
-
-//$(function () {
-//    $().timeline({
-//        orientation: 'vertical',
-//        issuesSpeed: 300,
-//        datesSpeed: 100,
-//        arrowKeys: 'true',
-//        startAt: 3
-//    })
-//});
