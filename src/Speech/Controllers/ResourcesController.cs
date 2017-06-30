d5391f4078df8510c2795870d6d5d0e75a70e28e
Nicole Sanders <nsanders9022@gmail.com>
2017-06-30 10:55:49 -0700

    add links to resources page and icons to footer

--- src/Speech/Controllers/ResourcesController.cs
+++ src/Speech/Controllers/ResourcesController.cs
@@ -13,5 +13,10 @@ namespace Speech.Controllers
         {
             return View();
         }
+
+        public IActionResult Resources()
+        {
+            return View();
+        }
     }
 }
