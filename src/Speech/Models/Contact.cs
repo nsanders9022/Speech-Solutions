b66e6b6cb484098147842532e45eb7f4d2b41c91
Nicole Sanders <nsanders9022@gmail.com>
2017-07-18 10:42:45 -0700

    add Contact model and cshtml page

---
+++ src/Speech/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Speech.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+    }
+}
